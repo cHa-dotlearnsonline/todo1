31b9434474ad1bc1fbba22d5ce9fb23ba3bbbc75
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2024-10-03 17:54:39 +0200

    Added a classes that allow me to add a category and an item to a todolist

--- mytodo/todolist/models.py
+++ mytodo/todolist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    cat_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.cat_name
+
+class TodoListItem(models.Model):
+    item = models.CharField(max_length=500)
