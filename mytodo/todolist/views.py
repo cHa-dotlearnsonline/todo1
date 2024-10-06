e80ac931e200c6c4268b6ef8adf73494fa5c857c
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2024-10-06 17:10:22 +0200

    I have now added an index.html page with the respective view

--- mytodo/todolist/views.py
+++ mytodo/todolist/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.template import loader
 
-
+from .models import Category, TodoListItem
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World. You are now at the ToDo List homepage")
+    latest_category_list = Category.objects.all()
+    output = "\n,".join([category.cat_name for category in latest_category_list])
+    context = {
+        "latest_cat_list" : latest_category_list,
+        }
+
+    return render(request, "todolist/index.html", context)
 
 def create_category(request):
     """ This allows users to create a category for which todo items will be added """
@@ -15,6 +22,6 @@ def view_category(request, category_id):
     """ This allows users to view items under each category"""
     pass
 
-def add_item(request):
-
+def add_item(request, category_id):
+    """ This allows the user to add an item to a specific category"""
     pass
